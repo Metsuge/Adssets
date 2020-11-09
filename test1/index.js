b90f5bb3539af224e5919b4c4574a2634a94e02b
Gustė Žilinskaitė <metsuge@gmail.com>
2020-11-09 16:23:51 +0200

    Add files via upload

--- test1/index.js
+++ test1/index.js
@@ -1,32 +1,78 @@
 const assert = require("chai").assert;
 
 const names = [
-    "David Erik Svensson",
-    "ELIN AMANDA gabriella SELLIN",
-    "Pippilotta Viktualia Rullgardina Krusmynta Efraimsdotter LÅNGSTRUMP",
-    "Kalle Anka",
-    "Ghandi"
+  "David Erik Svensson",
+  "ELIN AMANDA gabriella Sellin",
+  "Pippilotta Viktualia Rullgardina Krusmynta Efraimsdotter LÅNGSTRUMP",
+  "Kalle Anka",
+  "Ghandi",
 ];
 
 const expected = [
-    { first: "David", middle: ["Erik"], last: "Svensson" },
-    { first: "ELIN", middle: ["AMANDA", "gabriella"], last: "Sellin" },
-    { first: "Pippilotta", middle: ["Viktualia", "Rullgardina", "Krusmynta", "Efraimsdotter"], last: "LÅNGSTRUMP" },
-    { first: "Kalle", middle: [], last: "Anka" },
-    { first: "Ghandi", middle: [], last: null },
+  { first: "David", middle: ["Erik"], last: "Svensson" },
+  { first: "ELIN", middle: ["AMANDA", "gabriella"], last: "SELLIN" },
+  {
+    first: "Pippilotta",
+    middle: ["Viktualia", "Rullgardina", "Krusmynta", "Efraimsdotter"],
+    last: "LÅNGSTRUMP",
+  },
+  { first: "Kalle", middle: [], last: "Anka" },
+  { first: "Ghandi", middle: [], last: null },
 ];
 
-const validate = (expected) => {
-    try {
-        assert.deepEqual(expected, expected);
-        console.log("yay");
-    } catch (e) {
-        console.error("Failed", e);
-    }
+const validate = (result) => {
+  try {
+    assert.deepEqual(result, expected);
+    console.log("yay");
+  } catch (e) {
+    console.error("Failed", e);
+  }
 };
 
 // implement code generating result
-const result = [];
+
+let result = [];
+
+const completeObject = (names) => {
+  let nameSplit = [];
+
+  names.forEach((name) => {
+    nameSplit.push(name.split(" "));
+  });
+
+  for (let i = 0; i < nameSplit.length; i++) {
+    let EachNewObject = {
+      first: nameSplit[i][0],
+
+      middle: function () {
+        if (nameSplit[i].slice([1], nameSplit[i].length - 1).length === 0) {
+          EachNewObject.middle = [];
+        } else {
+          EachNewObject.middle = nameSplit[i].slice(
+            [1],
+            nameSplit[i].length - 1
+          );
+        }
+      },
+
+      last: function () {
+        if (nameSplit[i].length > 1) {
+          EachNewObject.last = nameSplit[i][nameSplit[i].length - 1];
+        } else if (nameSplit[i].length <= 1) {
+          EachNewObject.last = null;
+        }
+      },
+    };
+
+    EachNewObject.middle();
+    EachNewObject.last();
+    result.push(EachNewObject);
+  }
+  return result;
+};
+
+completeObject(names);
+// console.log(result);
 
 // At the end call validate
-validate(expected);
+validate(result);
