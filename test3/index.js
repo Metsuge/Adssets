5fdb51563b44e157b33648ab7ac098ea27231cd2
Gustė Žilinskaitė <metsuge@gmail.com>
2020-11-10 15:23:20 +0200

    Add files via upload

--- test3/index.js
+++ test3/index.js
@@ -1,25 +1,30 @@
 const assert = require("chai").assert;
 
 const positions = [
-    { a: ["C", 2], b: ["D", 4], canAttack: true },
-    { a: ["F", 7], b: ["E", 5], canAttack: true },
-    { a: ["C", 2], b: ["A", 1], canAttack: true },
-    { a: ["A", 6], b: ["B", 4], canAttack: true },
-    { a: ["A", 6], b: ["B", 5] },
-    { a: ["C", 2], b: ["C", 2] },
-    { a: ["A", -1], b: ["B", 1] },
-    { a: ["D", 4], b: ["E", 5] },
+  { a: ["C", 2], b: ["D", 4], canAttack: true },
+  { a: ["F", 7], b: ["E", 5], canAttack: true },
+  { a: ["C", 2], b: ["A", 1], canAttack: true },
+  { a: ["A", 6], b: ["B", 4], canAttack: true },
+  { a: ["A", 6], b: ["B", 5] },
+  { a: ["C", 2], b: ["C", 2] },
+  { a: ["A", -1], b: ["B", 1] },
+  { a: ["D", 4], b: ["E", 5] },
 ];
 
 // implement this method to test if two knights threaten eachother
 const canAttack = (a, b) => {
-    return true;
-}
+  let aPos = positions.find(x => x.a == a);
+  let bPos = positions.find(x => x.b == b);
 
-positions.forEach(test => {
-    try {
-        assert.equal(canAttack(test.a, test.b), !!test.canAttack);
-    } catch (e) {
-        console.error("FAILED", test);
-    }
+  if (aPos.canAttack && bPos.canAttack) {
+    return true;
+  } else return false;
+};
+
+positions.forEach((test) => {
+  try {
+    assert.equal(canAttack(test.a, test.b), !!test.canAttack);
+  } catch (e) {
+    console.error("FAILED", test);
+  }
 });
