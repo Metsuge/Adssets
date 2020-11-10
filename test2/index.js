700d18d40e1de102b70546849c0e5ce395e9b070
Gustė Žilinskaitė <metsuge@gmail.com>
2020-11-10 15:22:55 +0200

    Add files via upload

--- test2/index.js
+++ test2/index.js
@@ -33,6 +33,7 @@ const expected = [
     ],
   },
 ];
+
 const database = {
   621: { id: 621, name: "XxDragonSlayerxX", friends: [123, 251, 631] },
   123: { id: 123, name: "FriendNo1", friends: [621, 631] },
@@ -44,69 +45,49 @@ const result = [];
 const ids = [621, 123, 251, 631];
 const arrayOfObjects = [];
 let array = {};
+
 const getUser = (id, array) =>
   new Promise((res, rej) => {
     setTimeout(() => {
       database[id] ? res(database[id]) : rej(new Error("not_found"));
     }, 300);
-    array.friends = (database[id]);
-    arrayOfObjects.push(database[id])
+    array.friends = database[id];
+    arrayOfObjects.push(database[id]);
   });
 
-for(index in ids){
-    getUser(ids[index], array);
+for (index in ids) {
+  getUser(ids[index], array);
 }
-
-// console.log(arrayOfObjects[0].friends);
-const som = arrayOfObjects.find(x => x.id === 621);
-
 
 const getFullObject = (arrayOfObjects) => {
-  
-    for(let i=0;i<arrayOfObjects.length;i++){
-    let object = 
-        {
-            id: arrayOfObjects[i].id,
-            name: arrayOfObjects[i].name,
-            friends: function(){
-            let friendArray = arrayOfObjects[i].friends;
-            let rightFriends = [];
-            for(let i=0;i<friendArray.length;i++){
-            let fren = arrayOfObjects.find(x => x.id === friendArray[i])
-            rightFriends.push(fren);
-            }
-            return object.friends = rightFriends
-          }
-            
+  arrayOfObjects.forEach((each) => {
+    let object = {
+      id: each.id,
+      name: each.name,
+
+      friends: function () {
+        let friendArray = each.friends;
+        let rightFriends = [];
+        for (let i = 0; i < friendArray.length; i++) {
+          let fren = arrayOfObjects.find((x) => x.id === friendArray[i]);
+          rightFriends.push(fren);
         }
-    
-    object.friends()
-    console.log(object);
-}
-
-
-
-}
+        return (object.friends = rightFriends);
+      },
+    };
+    object.friends();
+    result.push(object);
+  });
+};
 
 getFullObject(arrayOfObjects);
 
+const validate = (result) => {
+  try {
+    assert.deepEqual(result, expected);
+  } catch (e) {
+    console.error("Failed", e);
+  }
+};
 
-
-  
-
-
-
-// const validate = (result) => {
-//     try {
-//         assert.deepEqual(result, expected);
-//     } catch (e) {
-//         console.error("Failed", e);
-//     }
-// };
-
-// List of user ids to test
-
-// implement a method to create this result
-
-// At the end call validate
-// validate(result);
+validate(result);
